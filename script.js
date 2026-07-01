98eeaee1a438bc2590262c19c501002906ce7514
kondratkoj <220639277+kondratkoj@users.noreply.github.com>
2026-07-01 17:20:17 -0500

    complete game logic to run in console.

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const gameController = (function() {
   let player1 = createPlayer("player1", "x");
   let player2 = createPlayer("player2", "o");
   let activePlayer = player1;
+  let gameWon = false;
 
   function createPlayer(name, symbol) {
     return {
@@ -28,13 +29,14 @@ const gameController = (function() {
       ["", "", "", 
        "", "", "", 
        "", "", ""];
+    gameWon = false;
+    activePlayer = player1;
     //create player1
     //create player2
   }
 
   function playRound (choice){
-    
-    if (Gameboard.array[choice] === "")  {
+    if (Gameboard.array[choice] === "" && gameWon === false)  {
       Gameboard.array[choice] = activePlayer.symbol;
       checkWin();
       switchPlayer();
@@ -42,31 +44,34 @@ const gameController = (function() {
   }
 
   function switchPlayer () {
-
     if (activePlayer == player1) {
       activePlayer = player2
     } else { activePlayer = player1};
-    return activePlayer;
   }
 
   function checkWin () {
-
     const winPatterns = [
       [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
       [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
       [0, 4, 8], [2, 4, 6]             // diagonals
     ];
+    
 
     for (const pattern of winPatterns) {
       const [a, b, c] = pattern;
-      
+
       if (Gameboard.array[a] !== "" && 
           Gameboard.array[a] === Gameboard.array[b] &&
           Gameboard.array[b] === Gameboard.array[c]) {
             alert(`${activePlayer.name} wins!`);
-          
-        }
+          gameWon = true;
+        } 
     }
+    const isTie = Gameboard.array.every(cell => cell !== "");
+    
+    if (gameWon === false && isTie) {alert("It's a tie!")};
+
+    return gameWon;
   }
 
   function getActivePlayer () {
@@ -95,6 +100,3 @@ function displayController () {
   // grabs dom elements
   // displays the state of elements
 };
-
-console.log(Gameboard.array);
-console.log(activePlayer);
