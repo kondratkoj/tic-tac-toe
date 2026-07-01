7f19488eccee7be7bb2c8a555c0126c5b8c89358
kondratkoj <220639277+kondratkoj@users.noreply.github.com>
2026-07-01 15:54:49 -0500

    I think I've got the pseudo code all ironed out and the logic put where it should be.

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const Gameboard = (function() {
 const gameController = (function() {
 
   let player1 = createPlayer("player1", "x");
-  let player2 = createPlater("player2", "o");
+  let player2 = createPlayer("player2", "o");
 
   function createPlayer(name, symbol) {
     return {
@@ -32,12 +32,30 @@ const gameController = (function() {
     //create player2
   }
 
-  function switchTurn () {
-    
+  function playRound (){
+    //check if spot is a number
+    //change spot to player symbol
+    //checkWin
+    //switchPlayer
+  }
+
+  function switchPlayer () {
+    //if move is valid (not clicking on a square already chosen)
+    //when symbol is changed, change player
   }
 
   function checkWin () {
-
+    //look for patterns in array 
+    //0, 1, 2
+    //3, 4, 5
+    //6, 7, 8
+    //0, 3, 6
+    //1, 4, 7
+    //2, 5, 8
+    //0, 4, 8
+    //2, 4, 6
+    //determin which player has those symbols
+    //that player wins
   }
 
   return {
@@ -45,18 +63,19 @@ const gameController = (function() {
     player1,
     player2,
     startGame,
-    switchTurn,
-    checkWin,
+    playRound,
+    switchPlayer,
+    checkWin
   }
 
 })();
 
+function playerInput () {
+    //grab dom elements for clicking
+    //return those elements
+  }
+
 function displayController () {
   // grabs dom elements
   // displays the state of elements
-}
-
-function playerInput () {
-  // grab dom elements
-  // changes element based on which player clicks it
-}
+};
